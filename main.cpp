a3492f785c7bcc896fc61bdfc2dc94c74802b13b
Awhyles <whylesalfie68@gmail.com>
2026-06-11 14:22:38 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    int x {};
+    int y {};
+    int z {};
+
+    std::cout << "Enter 3 numbers: ";
+    std::cin >> x >> y >> z;
+    std::cout << "Your numbers are: " << x << ", " << y << ", and " << z << ".\n";
+
+    return 0;
+}
